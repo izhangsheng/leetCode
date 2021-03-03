7265e164e39f7a457d8284c118d1146f688a4f14
izhangsheng <zhangs_0303@163.com>
2021-03-03 10:25:40 +0800

    快排

--- LeetCode/FindNearestAncestor.swift
+++ LeetCode/FindNearestAncestor.swift
@@ -7,19 +7,28 @@
 
 import Foundation
 
+class TreeNode {
+    var val: Int
+    var left: TreeNode?
+    var right: TreeNode?
+    init(val: Int, left: TreeNode?, right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
 class FindNearestAncestor {
-    class TreeNode {
-        var val: Int
-        var left: TreeNode?
-        var right: TreeNode?
-        init(val: Int, left: TreeNode?, right: TreeNode?) {
-            self.val = val
-            self.left = left
-            self.right = right
+    
+    func lowestCommonAncestor(root: TreeNode?, p: TreeNode, q: TreeNode) -> TreeNode? {
+        guard let root = root else {
+            return nil
         }
-        
-        convenience init(val: Int) {
-            self.init
+        if p.val == root.val || q.val == root.val {
+            return root
         }
-    };
+        let leftP = lowestCommonAncestor(root: root.left, p: p, q: q)
+        let rightP = lowestCommonAncestor(root: root.right, p: p, q: q)
+        return leftP == nil ? rightP : (rightP == nil ? leftP : root)
+    }
 }
--- LeetCode/main.swift
+++ LeetCode/main.swift
@@ -7,5 +7,63 @@
 
 import Foundation
 
-NQueue().solveNQueues(8)
+//NQueue().solveNQueues(8)
+
+findWrongNode()
+func findWrongNode() {
+//    if (prev != null && prev.val > node.val) {
+//        // 第2个错误节点：最后一个逆序对中较小的那个节点
+//        second = node;
+//
+//        // 第1个错误节点：第一个逆序对中较大的那个节点
+//        if (first != null) return;
+//        first = prev;
+//    }
+//    prev = node;
+    var arr = [9, 25, 6, 12, 8, 6]
+    
+    
+    func sort(elements: inout [Int]) {
+        quickSort(elements: &elements, begin: 0, end: elements.count)
+    }
+    
+    func quickSort(elements: inout [Int], begin: Int, end: Int) {
+        guard end - begin > 1 else {
+            return
+        }
+        let pivot = pivotIndex(sortArray: &elements, begin: begin, end: end)
+        quickSort(elements: &elements, begin: begin, end: pivot)
+        quickSort(elements: &elements, begin: pivot + 1, end: end)
+    }
+    
+    func pivotIndex(sortArray: inout [Int], begin: Int, end: Int) -> Int {
+        let pivotEle = sortArray[begin]
+        var left = begin
+        var right = end - 1
+        while right > left {
+            while right > left {
+                if sortArray[right] > pivotEle {
+                    right -= 1
+                } else {
+                    sortArray[left] = sortArray[right]
+                    left += 1
+                    break
+                }
+            }
+            while right > left {
+                if sortArray[left] < pivotEle {
+                    left += 1
+                } else {
+                    sortArray[right] = sortArray[left]
+                    right -= 1
+                    break
+                }
+            }
+        }
+        sortArray[left] = pivotEle
+        return left
+    }
+    sort(elements: &arr)
+    print(arr)
+}
 
