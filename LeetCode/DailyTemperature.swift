63a5ad54af3a023631725a06335377df945e36e9
zs1990 <zhangs_0303@163.com>
2021-03-13 16:35:08 +0800

    接雨水

--- LeetCode/DailyTemperature.swift
+++ LeetCode/DailyTemperature.swift
@@ -27,4 +27,35 @@ class DailyTemperature {
         
         return ascentList
     }
+    
+    /// 接雨水
+    /// - Parameter height: 所有柱子高度
+    /// - Returns: 总雨水量
+    public func trap(_ height: [Int]) -> Int {
+        let count = height.count
+        guard count > 2 else {
+            return 0
+        }
+        var dpRHeight = height
+        var end = count - 2
+        while end > 0 {
+            if dpRHeight[end + 1] < height[end + 1] {
+                dpRHeight[end] = height[end + 1]
+            }
+            dpRHeight[end] = max(dpRHeight[end + 1], height[end + 1])
+            end -= 1
+        }
+        
+        var leftMax = height[0]
+        var water = 0
+        for i in 1 ... count - 2 {
+            leftMax = max(leftMax, height[i - 1])
+            // 左右最小值
+            let minValue = min(leftMax, dpRHeight[i])
+            if minValue > height[i] {
+                water += minValue - height[i]
+            }
+        }
+        return water
+    }
 }
